c4fbc7b7c8f1216044489a6c9fed1b5a743c5d2e
Hazem Ben Hassine <hazem.benhassine@vynd.com>
2021-06-16 12:43:42 +0100

    Added register, login and edit functionalities.

--- users-service/src/user/user.service.spec.ts
+++ users-service/src/user/user.service.spec.ts
@@ -1,13 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schemas/user.schema';
+import { AuthModule } from '../auth/auth.module';
 
 describe('UserService', () => {
   let service: UserService;
 
+  const mockUserService = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UserService],
-    }).compile();
+    })
+      .overrideProvider(UserService)
+      .useValue(mockUserService)
+      .compile();
 
     service = module.get<UserService>(UserService);
   });
